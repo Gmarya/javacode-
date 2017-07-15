57118143fb3d17f591e945b1a95579fbde55bf88
sam <samthechamp001@gmail.com>
2017-07-16 02:05:43 +0530

    my first commit

---
+++ myPDF/src/main/java/com/mypkg/Testmain.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.mypkg;
+
+/**
+ * @author SAM
+ *
+ */
+public class Testmain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this os");
+        int a,b;
+        a=10;
+        b=10;
+        print(a,b);
+		
+	}
+
+	private static void print(int a, int b) {
+		// TODO Auto-generated method stub
+		System.out.println(a + "" + b);
+	}
+
+}
